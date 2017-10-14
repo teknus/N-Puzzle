3542f7a78be7ea34d931217a7e45db30a7fe61ef
teknus <mateusteknus@gmail.com>
2017-10-14 14:48:17 -0300

    Split archives

--- src/dfs.py
+++ src/dfs.py
@@ -1,4 +1,4 @@
-from skeletonNPuzzle import Matrix
+from Npuzzle import Matrix
 from os import path
 
 m = Matrix()
@@ -7,7 +7,7 @@ pth = path.abspath("")
 pth = pth[:len(pth)-3]
 m.loadMatrix(pth+"/matrix.txt")
 m2.loadMatrix(pth+"/matrix.txt")
-m.shuffleInstance(1)
+m.shuffleInstance(10)
 m2.shuffleInstance(2)
 m.show()
 print(m.possibleMoves)
